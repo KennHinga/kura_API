fc9245e0d7a0a582965ec6f5e3995dbb3574e4ff
kennHinga <hingaken@gmail.com>
2019-02-09 18:05:08 +0300

    [Finishes #163843437]corrected the tests so they passed

--- tests/office_test.py
+++ tests/office_test.py
@@ -1,28 +1,34 @@
 import json
 import unittest
-from app import create_app
+from run import app
 from app.api.v1.models.models import offices
 
 class TestOffices(unittest.TestCase):
 
     def setUp(self):
-        self.app = create_app(config_name='testing')
-        self.client = self.app.test_client
+        self.app = app
+        self.client= self.app.test_client
         self.data = {
             "name": "Governor",
-            "type": "elective"     
+            "office_type": "elective"     
         }
 
     def test_create_office(self):
-        response = self.client().post(path='/officeList', data=json.dumps(self.data), content_type='application/json')
-        self.assertEqual(resp.status_code, 201)
-    
+        response = self.client().post(path='/api/v1/officeList', data=json.dumps(self.data), content_type='application/json')
+        resp = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
     
     def test_get_all_offices(self):
-        response = self.client().post(path='/officeList', data=json.dumps(self.data), content_type='application/json')
-        self.assertEqual(resp.status_code, 200)
+        self.client().post(path='/api/v1/officeList', data=json.dumps(self.data), content_type='application/json')
+        response = self.client().get(path='/api/v1/officeList', content_type="application/json")
+        self.assertEqual(response.status_code, 200)
 
-    def test_offices_get_one(self):
-        # response = self.client().post(path='/partyList', data=json.dumps(self.data), content_type='application/json')
-        response = self.client().get('/officelist/1', content_type="application/json")
-        self.assertEqual(resp.status_code, 200)
+    def test_ofices_get_one(self):
+        self.client().post(path='/api/v1/officeList', data=json.dumps(self.data), content_type='application/json')
+        response = self.client().get(path='/api/v1/officeList/1', content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        
+
+if __name__ == '__main__':
+    unnittest.main()
+
