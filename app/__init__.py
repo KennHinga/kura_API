c95bcb54a79e94cd8e65ec60e278cc5ee8b7714a
kennHinga <hingaken@gmail.com>
2019-02-15 09:43:40 +0300

    [Starts #163915913] sign up feature for a user

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask
 from app.api.v1.views.views import version_one as v1
-from app.api.v2.Views.user_views import version_two as v2
 
 def create_app(config_name):
     """create the flask application"""
     app = Flask(__name__)
     app.register_blueprint(v1, url_prefix="/api/v1/")
-    app.register_blueprint(v2, url_prefix="/api/v2/")
-    # app.url_map,strict_slashes = False
     return app
--- app/api/v2/Views/user_views.py
+++ app/api/v2/Views/user_views.py
@@ -1,30 +1,42 @@
-import os
 import datetime
-import jwt
-from app.api.v2.Models.user_models import UserModel 
-from flask import Blueprint, request, jsonify, abort, make_response
+import psycopg2
+from .db import DbConnection as db_conn
+from passlib.hash import pbkdf2_sha256
 
-version_two = Blueprint('version_two', __name__, url_prefix= '/api/v2' )
+TIME_NOW = datetime.datetime.utcnow()
 
-class Register:
-    """class for signing up to become a user"""
+class UserModel(db_conn):
+    """class for User"""
 
-    @version_two.route('/register', methods=['POST'])
-    def post_user():
-        data = request.get_json()
-        firstName = data['firstName']
-        lastName = data['lastName']
-        otherName = data['otherName']
-        email = data['email']
-        phoneNumber = data['phoneNumber']
-        passportUrl = data['passportUrl']
-        isAdmin = data['isAdmin']
-        isCandidate = data['isCandidate']
-        password = data['password']
+    def __init__(self, user={}):
+        self.db=db_conn()
+        self.user= user
 
-        user= UserModel().create_new_user(firstName, lastName, otherName, email, phoneNumber, passportUrl, isAdmin, isCandidate, password)
-        
-        return make_response(jsonify({
-            "message": "user created successfully",
-            "data": user
-        }),201)
+    def create_new_user(self, firstName, lastName, otherName, email, phoneNumber, passportUrl, isAdmin, isCandidate, password):
+        """ Adds a new user to the users table"""
+        user={
+            "firstName": firstName,
+            "lastName": lastName,
+            "otherName": otherName,
+            "email": email,
+            "phoneNumber": phoneNumber,
+            "passportUrl": passportUrl,
+            "isAdmin": isAdmin,
+            "isCandidate": isCandidate,
+            # "password": user.generate_hash(password)
+        }
+
+        query = """ INSERT INTO users( firstName, lastName, otherName, email, phoneNumber, passportUrl, isAdmin, isCandidate, password)
+                VALUES(%(firstName)s, %(lastName)s, %(otherName)s, %(email)s, %(phoneNumber)s, %(passprtUrl)s, %(isAdmin)s, %(isCandidate)s, %(password)s)"""    
+        # cursor= self.db.cursor()
+        # cursor.execute(query, user)
+        # self.db.commit()
+        return user
+
+    @staticmethod
+    def generate_hash(password):
+        return sha256.hash(password)
+    
+    @staticmethod
+    def hash_verify(password, hash):
+        return sha256.verify(password, hash)
